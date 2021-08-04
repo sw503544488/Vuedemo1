cf1ce7692108e2467784902d5563481e560ee8eb
sw503544488 <9531592+sw503544488@user.noreply.gitee.com>
2021-08-04 23:11:21 +0800

    “vuedemo”

---
+++ src/Demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="red">
+    {{ message }}
+    <button @click="add">+1</button>
+
+  </div>
+</template>
+<script>
+export default {
+  props: ['message']
+}
+</script>
+<style scoped>
+.red {
+  color: gray;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,51 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+const Vue = window.Vue
 Vue.config.productionTip = false
+import Demo from "./Demo";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    data: {
+        visible: true
+    }, template: `
+    <div class="frank">
+    {{ n }}
+    <button @click='add'>
+      +1
+    </button>
+    <hr>
+    </div>
+    `,
+    created() {
+        console.log('实例出现在内存中')
+    },
+    mounted() {
+        console.log('实例出现在页面中')
+    },
+    updated() {
+        console.log('更新了')
+    },
+    methods: {
+        add() {
+            this.n += 1
+        }
+    }
+
+}).$mount('#frank')
+
+new Vue({
+    data: {visible: true},
+    components: {Demo},
+    template: `
+      <div>
+      <button @click="toggle" v-if="visible===true">toogle</button>
+      <hr>
+      <Demo message="你好props"/>
+      </div>
+    `,
+    methods: {
+        toggle() {
+            this.visible = !this.visible
+        }
+    }, destroyed() {
+        console.log('被吹毁')
+    }
+}).$mount('.app')
